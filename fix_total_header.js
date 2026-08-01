5995ae636127d900cb9cf7e7613b7b265b2813da
YonnyAHM <datapromns@gmail.com>
2026-08-01 07:00:31 -0400

    v1.9.18.78

--- fix_total_header.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const file = 'C:/Users/yonny.h/Videos/DATA/Data Pro V1.9/src/app/page.tsx';
-let content = fs.readFileSync(file, 'utf8');
-const lines = content.split('\n');
-lines[3069] = null;
-const newLines = lines.filter(l => l !== null);
-fs.writeFileSync(file, newLines.join('\n'));
-console.log('removed Total header');
--- src/hooks/use-permissions-store.ts
+++ src/hooks/use-permissions-store.ts
@@ -132,6 +132,7 @@ const DEFAULT_PERMISSIONS: UserPermissions = {
   'procj.mds': ['planning'],
   'cald.mds': ['planning'],
   'prodt.mds': ['planning', 'planta'],
+  'prodt1.mds': ['planning', 'planta'],
   'proc.mds': ['planning', 'planta'],
   'g.tec.mds': ['planning', 'seguimiento', 'management'],
   'enf.mds': ['planning', 'seguimiento', 'management'],
